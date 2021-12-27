c223049670259350b520e939edcea76f858c3673
ashacklady <andyshacklady@gmail.com>
2021-12-27 13:03:28 +0000

    v2.0.2

--- calc/math.go
+++ calc/math.go
@@ -1,5 +1,7 @@
 package calc
 
+import "errors"
+
 // func Add(i, j int) int {
 // 	return i + j
 // }
@@ -12,10 +14,10 @@ package calc
 // 	return sum
 // }
 
-func Add(numbers ...int) (string, int) {
+func Add(numbers ...int) (error, int) {
 	sum := 0
 	if len(numbers) < 2 {
-		return "provide more than 2 numbers", sum
+		return errors.New("provide more than 2 numbers"), sum
 	}
 	for _, num := range numbers {
 		sum += num
