fb9e5df59b99bf4660bbb5d84530b5064f6b329c
ashacklady <andyshacklady@gmail.com>
2021-12-27 12:21:25 +0000

    new impl of calc

--- calc/math.go
+++ calc/math.go
@@ -1,5 +1,13 @@
 package calc
 
-func Add(i, j int) int {
-	return i + j
+// func Add(i, j int) int {
+// 	return i + j
+// }
+
+func Add(numbers ...int) int {
+	sum := 0
+	for _, num = range numbers {
+		sum += num
+	}
+	return sum
 }
